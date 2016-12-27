6408baa8169cb4adeff229a4b6241a0cf50468ab
Anish-Jajodia <hello@lomdi.in>
2016-12-27 17:01:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
-echo "welcome to my app";
+session_start();
+// Required File Start.........
+require __DIR__.'/conf.php'; //Configuration
+require __DIR__.'/connection.php'; //DB connectivity
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+// Required File END...........
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// require __DIR__.'/get_products.php'; //GET PRODUCTS
+//echo $_REQUEST['code'];
+if((isset($_REQUEST['shop'])) && (isset($_REQUEST['code'])) && $_REQUEST['shop']!='' && $_REQUEST['code']!='' )
+{
+	$_SESSION['shop']=$_REQUEST['shop'];
+	$_SESSION['code']=$_REQUEST['code'];
+}
+$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+
+require __DIR__.'/orders.php'; //create smart collection
 
 ?>
