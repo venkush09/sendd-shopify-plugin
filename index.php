b177c680ee928d17b9b47520904cb141809248e7
Anish-Jajodia <hello@lomdi.in>
2016-12-28 18:59:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ function getorders(){
 		function sliderInit(){
 	 $('.popupcontent_inner').slick({
 	 slidesToShow: 1,
-		slidesToScroll:1,
+		
 		dots: true,
 		infinite: false,
 		variableWidth: true
