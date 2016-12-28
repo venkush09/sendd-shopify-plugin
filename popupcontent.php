e9004ca00392d7523fa607bf924bc66ddeaa881e
Anish-Jajodia <hello@lomdi.in>
2016-12-28 10:48:32 +0530

    Update popupcontent.php

--- popupcontent.php
+++ popupcontent.php
@@ -1,1 +1,21 @@
-<?php echo "<div id='popup_content'>test</div>"; ?>
+<h3>Shipping information</h3>
+<label>Pickup Address*</label>
+<select name="pickup_address">
+<option>AMIT KAUSHAL:h.no.88, top floor, gali no.2neb sarai,South West Delhi,Delhi- 110068</option>
+<option>Mohini Ranjan Meher:HIG 26 BDA Complex, Jaydev ViharOpp Pal Helght,Khorda,Odisha- 751013</option>
+<option>Nidall Abdul Aziz:4A3, Green Terrace ApartmentsSeaport Airport Road, Kochi,Ernakulam,Kerala- 682021</option>
+<option>Harshit Jaswani:567/B sector k aashiyana colonyNear chiranjiv bharti park,Lucknow,Uttar Pradesh- 226012 </option>
+<option>Mohamed Athif:Kottathara Nalakath House ,B.P Angadi POVia ,Malappuram Dist,Malappuram,Kerala- 676102 </option>
+<option>Shashank Nagrale:A-103, KPR Elite Apartment, Kasavanahalli Main Road, Off SarNear Andhra Bank,Bangalore,Karnataka- 560035</option>
+<option>Amit Malik:House number 142, Room number 1Katwaria Sarai, Hauz Khas,South West Delhi,Delhi- 110016</option>
+<option>Naveen Kishore:1/297, Rajaji nagar, 1st cross, Royakotta main RoadNear MGM palace logde,,Krishnagiri,Tamil Nadu- 635001</option>
+<option>Krishna Kanth Jaju:Ganesh boys hostel, room no F3Datta Meghe institute of medical sciences,Wardha,Maharashtra- 442001</option>
+<option>Abhijeet Ranpise:102, Rutu business park, majiwada service roadAbove ccd and Pizza Hut, thane west,Thane,Maharashtra- 400601<option>
+</select>
+<label>Drop address* </label>
+<textarea  class="drop_address"  required=""></textarea>
+<label>Payment Type</label>
+<div id="payment_type"></div>
+<?php
+
+?>
