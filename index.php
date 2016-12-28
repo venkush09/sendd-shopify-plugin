89ed8cb1fc83391354d4808fcc7e3ce5a7076ffe
Anish-Jajodia <hello@lomdi.in>
2016-12-28 12:54:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,11 @@ if((isset($_REQUEST['shop'])) && (isset($_REQUEST['code'])) && $_REQUEST['shop']
 $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
 ?>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-<script src="jquery.fancybox.js"></script>
-<link rel="stylesheet" href="style.css" type="text/css">
-<link rel="stylesheet" href="jquery.fancybox.css" type="text/css">
+<script src="js/jquery.fancybox.js"></script>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<script src="js/owl.carousel.min.js"></script>
+<link rel="stylesheet" href="css/carousel.css" type="text/css">
+<link rel="stylesheet" href="css/jquery.fancybox.css" type="text/css">
 
 <div class="content-container"></div>
 
@@ -57,8 +59,8 @@ function getorders(){
 	var customer_address = $(this).attr('data-address');
 	var payment_method = $(this).attr('data-gatway');
 	var content ='<div class="items"><h3>Shipping information</h3><label>Pickup Address*</label><select name="pickup_address"><option>AMIT KAUSHAL:h.no.88, top floor, gali no.2neb sarai,South West Delhi,Delhi- 110068</option><option>Mohini Ranjan Meher:HIG 26 BDA Complex, Jaydev ViharOpp Pal Helght,Khorda,Odisha- 751013</option><option>Nidall Abdul Aziz:4A3, Green Terrace ApartmentsSeaport Airport Road, Kochi,Ernakulam,Kerala- 682021</option><option>Harshit Jaswani:567/B sector k aashiyana colonyNear chiranjiv bharti park,Lucknow,Uttar Pradesh- 226012 </option><option>Mohamed Athif:Kottathara Nalakath House ,B.P Angadi POVia ,Malappuram Dist,Malappuram,Kerala- 676102 </option><option>Shashank Nagrale:A-103, KPR Elite Apartment, Kasavanahalli Main Road, Off SarNear Andhra Bank,Bangalore,Karnataka- 560035</option><option>Amit Malik:House number 142, Room number 1Katwaria Sarai, Hauz Khas,South West Delhi,Delhi- 110016</option><option>Naveen Kishore:1/297, Rajaji nagar, 1st cross, Royakotta main RoadNear MGM palace logde,,Krishnagiri,Tamil Nadu- 635001</option><option>Krishna Kanth Jaju:Ganesh boys hostel, room no F3Datta Meghe institute of medical sciences,Wardha,Maharashtra- 442001</option><option>Abhijeet Ranpise:102, Rutu business park, majiwada service roadAbove ccd and Pizza Hut, thane west,Thane,Maharashtra- 400601<option></select>';
-	 content = content + '<input type="text" class="customer_name" value="'+customer_name+'"><br><input type="text" class="customer_email" value="'+customer_email+'"><br><textarea class="customer_address" value="'+customer_address+'">'+customer_address+'</textarea>';
-	 content = content + '<label>Payment Type:</label><p>"'+payment_method+'"</p></div>';
+	 content = content + '<label>Customer Name:</label><input type="text" class="customer_name" value="'+customer_name+'"><br><label>Customer Email:</label><input type="text" class="customer_email" value="'+customer_email+'"><br><label>Customer Address:</label><textarea class="customer_address" value="'+customer_address+'">'+customer_address+'</textarea>';
+	 content = content + '<br><label>Payment Type:</label><p>"'+payment_method+'"</p></div>';
 
 
 	$('.popupcontent_inner').append(content);
