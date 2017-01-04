5e9b8a3e6e8940a0c537f5835897eb3e698a6b09
Anish-Jajodia <hello@lomdi.in>
2017-01-04 17:45:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -178,11 +178,11 @@ $('body').on('click', 'a.Create_order', function(e) {
 	   
 		       	var request = new XMLHttpRequest();
 
-			request.open('POST', 'https://api-staging.sendd.co/core/api/v1/order/');
+			request.open('POST', 'https://api.sendd.co/core/api/v1/order/');
 
 			request.setRequestHeader('Content-Type', 'application/json');
-			request.setRequestHeader('Authorization', 'Token 39757c4c7867f048ed452812df9f4d7395842de8');
-                            alert("token1");
+			request.setRequestHeader('Authorization', 'Token 0eb688db8076a89861b3885a9cccdcc30edc7a0e');
+                            alert("token12");
 			request.onreadystatechange = function () {
 			  if (this.readyState === 4) {
 				console.log('Status:', this.status);
