50c26edbb362cb8ba09854ebc9d8c1d98bb9b852
Anish-Jajodia <hello@lomdi.in>
2017-01-05 16:17:47 +0530

    Update popupcontent.php

--- popupcontent.php
+++ popupcontent.php
@@ -1,4 +1,4 @@
 <div id="popup_content" style="display:none;">
-  <a href="javascript:void(0)" onclick="closepopup();"> Close</a>
+  <div class="popup_close"><a href="javascript:void(0)" onclick="closepopup();"> Close</a></div>
 
 </div>
