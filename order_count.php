3b3f531011085f1ad37710a13604013d8ddb6b1f
Anish-Jajodia <hello@lomdi.in>
2017-01-05 12:28:28 +0530

    Update order_count.php

--- order_count.php
+++ order_count.php
@@ -7,6 +7,7 @@ use phpish\shopify;
 // Required File END...........
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+$access_token=$_REQUEST['access_token'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 $order_count = $shopify('GET /admin/orders/count.json?fulfillment_status=unshipped');
 	$limit=20; // Number of order per page
