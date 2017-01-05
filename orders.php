a09089b41d61ce6e8aafbf677a002af57f01ec52
Anish-Jajodia <hello@lomdi.in>
2017-01-05 11:50:21 +0530

    Update orders.php

--- orders.php
+++ orders.php
@@ -66,7 +66,7 @@ try
 					}
 				
 				echo "<tr>";
-				echo "<td><input type='checkbox' class='select_box' name='order_ids_$id'  value='$id'  data-address1='$address' data-address2='$address2' data-city='$city' data-zip='$zip' data-province='$province' data-total_weight='$total_weight' data-quantity_total='$quantity_total' data-customer_total-price='$total_price' data-customer_email='$email' data-customer_name='$customer_name' data-address='$full_address' data-gateway='$gateway' data-customer_phone='$customer_phone'></td>";
+				echo "<td><input type='checkbox' class='select_box' name='order_ids_$id'  value='$id'  data-address1='$address' data-address2='$address2' data-city='$city' data-zip='$zip' data-province='$province' data-total_weight='$total_weight' data-quantity_total='$quantity_total' data-customer_total-price='$total_price' data-customer_email='$email' data-customer_name='$customer_name' data-fulladdress='$full_address' data-gateway='$gateway' data-customer_phone='$customer_phone'></td>";
 				echo "<td>".$name."</td>";
 				echo "<td>".$created_at."</td>";
 				echo "<td>".$customer_name."</td>";
