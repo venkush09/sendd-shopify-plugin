6b4c534ad95471e627975d820815b4fffbaa603d
Anish-Jajodia <hello@lomdi.in>
2017-04-15 15:37:58 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -9,11 +9,9 @@ require __DIR__.'/connection.php'; //DB connectivity
 	//convert string to md5
 	//$password 	= md5($password);
 	echo $shop_url = $_SESSION['shop'];
-		echo "hello124";
+		echo "hello1245";
 	 //print_r($dbconn4);
-   $result = pg_query($dbconn4,"SELECT * FROM user_table"); 
-print_r($result);
-   $select_store = mysqli_query($dbconn4,"SELECT email,password FROM user_table WHERE store_url = '$shop_url'");
+ $select_store = mysqli_query($dbconn4,"SELECT email,password FROM user_table WHERE store_url = '$shop_url'");
 print_r($select_store);
 	if (mysqli_num_rows($select_store) > 0) {
 		$data = mysqli_fetch_assoc($select_store);
