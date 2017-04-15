ac54f85afb1dc4c113d8ddf89c5696c3d138b667
Anish-Jajodia <hello@lomdi.in>
2017-04-15 14:40:30 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,12 +1,12 @@
 <?php 
 	echo "hello";
-	/*$email= mysql_real_escape_string($_POST['email']);
-	$password= mysql_real_escape_string($_POST['password']);
+	$email= $_POST['email'];
+	$password= $_POST['password'];
         echo "<script>alert(1);</scripr>";
 	echo "$email=".$email;
-		die();
+		
 	//convert string to md5
-	$password 	= md5($password);
+	/*$password 	= md5($password);
 	
 	$sql = "select email, password from user_table where email = '$email' and password = '$password'";
 	$qry = mysql_query($sql);
