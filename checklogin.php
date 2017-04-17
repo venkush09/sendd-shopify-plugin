e78c28b35791999883d08da33feddbd14594ad0a
Anish-Jajodia <hello@lomdi.in>
2017-04-17 13:59:27 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -8,8 +8,8 @@ require __DIR__.'/connection.php'; //DB connectivity
 	//convert string to md5
 	//$password 	= md5($password);
 	$shop_url = $_SESSION['shop'];
-	$user_exist = pg_query($dbconn4, "SELECT email,password  FROM user_table WHERE store_url = '$shop_url' and email = '$email' and password = '$password'");
-	print_r($user_exist);
+	$user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}' and email = '{$email}' and password = '{$password}'");
+	
 if(pg_num_rows($user_exist)){
 		while($response = pg_fetch_assoc($user_exist)){
 			$_SESSION['email'] = $email;
