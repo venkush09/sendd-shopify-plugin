6b1ad6b27d7ead22950e7150ad6aee067a50e36f
Anish-Jajodia <hello@lomdi.in>
2017-04-17 13:47:29 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -8,42 +8,18 @@ require __DIR__.'/connection.php'; //DB connectivity
 	//convert string to md5
 	//$password 	= md5($password);
 	echo $shop_url = $_SESSION['shop'];
-		echo "hello123";
-$shop_exists = pg_query($dbconn4, "SELECT * FROM user_table1");
-
-$lastID = pg_fetch_assoc($shop_exists);
-
-if(pg_num_rows($shop_exists)){
-				while($response = pg_fetch_assoc($shop_exists)){
+		
+$user_exist = pg_query($dbconn4, "SELECT email,password  FROM user_table WHERE store_url = '$shop_url' and email = '$email' and password = '$password'");
+if(pg_num_rows($user_exist)){
+				while($response = pg_fetch_assoc($user_exist)){
+					$_SESSION['email'] = $email;
 					$json = $response['value'];
-					if(!empty($json)){
-						echo "{$response['key']}:: <pre>";
-						print_R($json);
-						echo "</pre>";
+					echo "cool";
 					}
 				}
 			}
-/* $select_store = pg_query($dbconn4,"SELECT email,password FROM user_table WHERE store_url = '$shop_url' and email = '$email' and password = '$password'");
-print_r($select_store);
-	if (pg_num_rows($select_store) > 0) {
-		$data = mysqli_fetch_assoc($select_store);
-		$_SESSION['email'] = $email;
-		echo "cool";
-			}*/
- $select_store2 = mysqli_query($dbconn4,"SELECT * FROM user_table");
-print_r( $select_store2);
-	/*$sql = "select email, password from user_table where email = '$email' and password = '$password'";
-	$qry = mysql_query($sql);
 
-	$numrows  = mysql_num_rows($qry);
-	
-	if($numrows > 0){
-		
-		//set seesion
-		echo $_SESSION['email'] = $email;
-		//print message for jquery
-		echo "cool";
-	}*/
+
 	
 
 ?>
