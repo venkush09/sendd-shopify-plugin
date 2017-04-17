f3ea88efb4174c5f67c1812174f78610ea72c6ae
Anish-Jajodia <hello@lomdi.in>
2017-04-17 17:49:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,14 +30,7 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
  <script src="js/jquery.twbsPagination.js" type="text/javascript"></script>
 <div class="background_overlay" style="display:none"></div>
 <div class="page"></div>
-
-<?php $check_login_status = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$_SESSION['shop']}' and  status = '1'"); ?>
-
-<div class="login-form-main" <?php if((pg_num_rows($check_login_status)) < 0 ) { echo 'style="display:none"'; } else {echo 'style="display:block"';}?>>
-	<?php include 'login-reg-form.php';?></div>
-
-<div class="content-container" <?php if(!(pg_num_rows($check_login_status)) < 0) { echo 'style="display:none"'; } else {echo 'style="display:block"';}?>></div>
-
+<div class="content-container" ></div>
 <script>
 	// Get orders
 	function getorders(page,limit){
