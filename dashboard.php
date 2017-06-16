81d3a151b3cbb824b7937f55127e50261d6dae2f
Anish-Jajodia <hello@lomdi.in>
2017-06-16 16:32:38 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 
-<ul>
+<ul class="page_list">
   <li><a href="javascript:void(0)" data-show="loginform" class="selected">Login Form</li>
   <li><a href="javascript:void(0)" data-show="orderform">Order Page</a></li>
 </ul>
