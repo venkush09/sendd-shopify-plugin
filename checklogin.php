987a26ebd8af498dd1d7ccfc7ef81e688f875694
Anish-Jajodia <hello@lomdi.in>
2017-06-16 16:48:30 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -2,8 +2,8 @@
 session_start();
 require __DIR__.'/connection.php'; //DB connectivity
 	
-	echo  $access_key= $_POST['access_key'];
-	echo $shop_url = $_POST['shop_url'];
+	 $access_key= $_POST['access_key'];
+	$shop_url = $_POST['shop_url'];
 	if(isset($_REQUEST['access_key']) || $_REQUEST['access_key']!=''){
 	 $user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}' and  access_key = '{$access_key}'");
 	if(pg_num_rows($user_exist)){
