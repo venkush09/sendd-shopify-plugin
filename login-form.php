8b8c44fe516dc0edaf5862092b7a211427d16fca
Anish-Jajodia <hello@lomdi.in>
2017-06-16 13:05:45 +0530

    Update login-form.php

--- login-form.php
+++ login-form.php
@@ -1,1 +1,8 @@
 
+<div id="login_form">
+	<h3>Login with your Sendd shipping login credentials</h3>
+	<label>Email</label><input type="text" name="email" id="email"/><br />
+	<label>Password</label><input type="password" name="password" id="password"/><br/>
+		<div class="msg">&nbsp;</div>
+	<button id="login">Login</button> 
+</div>
