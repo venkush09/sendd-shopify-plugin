f8d3bc66b4e376d03a17272774fa32e0a47a1f72
Anish-Jajodia <hello@lomdi.in>
2017-06-16 16:03:12 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -2,17 +2,17 @@
 session_start();
 require __DIR__.'/connection.php'; //DB connectivity
 	
-	echo  $access_key= $_POST['access_key'];
-	echo $shop_url = $_POST['shop_url'];
+	 $access_key= $_POST['access_key'];
+	 $shop_url = $_POST['shop_url'];
 	if(isset($_REQUEST['access_key']) || $_REQUEST['access_key']!=''){
 	 $user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}' and  access_key = '{$access_key}'");
 	if(pg_num_rows($user_exist)){
-		$user_exist = pg_query($dbconn4, "UPDATE user_table SET access_key = '{$access_key}' WHERE store_url = '{$shop_url}'");
+		$user_exist = pg_query($dbconn4, "UPDATE user_table SET access_key ='{$access_key}' WHERE store_url = '{$shop_url}'");
 			print_r($user_exist);
 		}
 		else {
 			$sql = "insert into user_table (access_key, store_url) values ('$access_key', '$shop_url')";
-			$qry = mysql_query($sql);
+			$qry = pg_query($sql);
 			if($qry){
 				echo "cool";
 			}
