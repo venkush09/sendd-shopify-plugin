186fd8fdfd543cacd95330b59548ed9d75a7fbc9
Anish-Jajodia <hello@lomdi.in>
2017-06-16 17:09:55 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -6,11 +6,11 @@ require __DIR__.'/connection.php'; //DB connectivity
 	$shop_url = $_POST['shop_url'];
 	if(isset($_REQUEST['access_key']) || $_REQUEST['access_key']!=''){
 	 $user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}' and  access_key = '{$access_key}'");
-	if(pg_num_rows($user_exist)){
-		$user_exist = pg_query($dbconn4, "UPDATE user_table SET access_key ='{$access_key}' WHERE store_url = '{$shop_url}'");
-			if($user_exist){
-				echo "cool";
-			}
+		if(pg_num_rows($user_exist)){
+			$user_exist = pg_query($dbconn4, "UPDATE user_table SET access_key ='{$access_key}' WHERE store_url = '{$shop_url}'");
+				if($user_exist){
+					echo "cool";
+				}
 		}
 		else {
 			$sql = "insert into user_table (access_key, store_url) values ('$access_key', '$shop_url')";
@@ -18,6 +18,13 @@ require __DIR__.'/connection.php'; //DB connectivity
 			if($qry){
 				echo "cool";
 			}
+		}
+	}
+	else{
+		$user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}'");
+		if(pg_num_rows($user_exist)){
+			echo "cool";
+				
 		}
 	}
 ?>
