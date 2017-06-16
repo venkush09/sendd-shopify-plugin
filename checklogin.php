8febbad7818613307e3cf9a651ab0e2a2302d79e
Anish-Jajodia <hello@lomdi.in>
2017-06-16 17:33:34 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -21,10 +21,13 @@ require __DIR__.'/connection.php'; //DB connectivity
 		}
 	}
 	else{
-		$user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}'");
+		$user_exist = pg_query($dbconn4, "SELECT access_key FROM user_table WHERE store_url = '{$shop_url}'");
 		if(pg_num_rows($user_exist)){
 			echo "cool";
-				
+			while($response = pg_fetch_assoc($user_exist)){
+					$json = $response['value'];
+					echo "api_key=".$json;
+				}
 		}
 	}
 ?>
