2f2900835581fa474c8dc5059c896b818eef30bf
Anish-Jajodia <hello@lomdi.in>
2017-06-17 12:00:58 +0530

    Update Pickup_address.php

--- Pickup_address.php
+++ Pickup_address.php
@@ -1,1 +1,14 @@
 
+<div class="pickupadreess">
+  <div class="address_1">
+  <form action="" method="POST">
+    <input type="hidden" value="address 1">
+    <input type="text" name="username" required>
+    <input type="textarea" name="address_line1" required max="60">
+    <input type="textarea" name="address_line2"  max="60">
+    <input type="text" name="zipcode" required>
+    <input type="text" name="phoneno" required>
+    <input type="submit" name="address1" value="Save Address">
+  </form>
+  </div>
+</div>
