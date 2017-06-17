35902ad947486826d9a54f0dc7064c936448b954
Anish-Jajodia <hello@lomdi.in>
2017-06-17 12:12:58 +0530

    Update Pickup_address.php

--- Pickup_address.php
+++ Pickup_address.php
@@ -1,7 +1,14 @@
+<script>
+jQuery("#add_new_address").click(function() {
+		var address_id = parseInt(jQuery('.addnewaddress').length+1);	 
+		var planDiv = ' <form action="" method="POST" id="form_'+address_id+'"><input type="hidden" value="address '+address_id+'"><input type="text" name="username" required><input type="textarea" name="address_line1" required max="60"><input type="textarea" name="address_line2"  max="60"><input type="text" name="zipcode" required><input type="text" name="phoneno" required> <input type="submit" name="address'+address_id+'" value="Save Address"></form>'; 
+		jQuery("div[class^=addnewaddress]:last").after(planDiv);
+	});
 
+</script>
 <div class="pickupadreess">
   <div class="address_1">
-  <form action="" method="POST">
+  <form action="" method="POST" id="form_1">
     <input type="hidden" value="address 1">
     <input type="text" name="username" required>
     <input type="textarea" name="address_line1" required max="60">
@@ -11,4 +18,7 @@
     <input type="submit" name="address1" value="Save Address">
   </form>
   </div>
+  <div class="addnewaddress"></div>
+	<br/>
+	<input type="button" id="add_new_address" value="Add New Address" /><br/> 
 </div>
