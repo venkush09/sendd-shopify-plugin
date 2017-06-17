68cbe8d4c5a1b8c26653ab039844931277c0262d
Anish-Jajodia <hello@lomdi.in>
2017-06-17 12:14:28 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,5 +2,5 @@
 <ul class="page_list">
   <li><a href="javascript:void(0)" data-show="loginform" class="selected">Login Form</li>
   <li><a href="javascript:void(0)" data-show="orderform">Order Page</a></li>
-  <li><a href="javascript:void(0)" data-show="pickupform">Pick Address</a></li>
+  <li><a href="javascript:void(0)" data-show="pickupaddress">Pick Address</a></li>
 </ul>
