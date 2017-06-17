8eac436c91e174982eb87bd17cb39f9d0d19781e
Anish-Jajodia <hello@lomdi.in>
2017-06-17 12:34:53 +0530

    Update Pickup_address.php

--- Pickup_address.php
+++ Pickup_address.php
@@ -1,12 +1,14 @@
 <div class="pickupadreess">
-  <div class="address_1">
+<div class="address_1">
+<h3>Pickup Address 1</h3>
   <form action="" method="POST" id="form_1">
     <input type="hidden" value="address 1">
-    <input type="text" name="username" required>
-    <input type="textarea" name="address_line1" required max="60">
-    <input type="textarea" name="address_line2"  max="60">
-    <input type="text" name="zipcode" required>
-    <input type="text" name="phoneno" required>
+    <input type="text" name="username" placeholder="Name" required>
+    <input type="textarea" name="address_line1" placeholder="Adrress 1" required max="60">
+    <input type="textarea" name="address_line2" placeholder="Adrress 2"  max="60">
+	<input type="text" name="city" placeholder="City" required>
+    <input type="text" name="zipcode" placeholder="Zip Code" required>
+    <input type="text" name="phoneno" placeholder="Phone no" required>
     <input type="submit" name="address1" value="Save Address">
   </form>
   </div>
@@ -19,7 +21,7 @@ $("#add_new_address").click(function() {
 	alert(1);
 		var address_id = parseInt($('.addnewaddress').length+1);	
 	alert(1);
-		var planDiv = ' <form action="" method="POST" id="form_'+address_id+'"><input type="hidden" value="address '+address_id+'"><input type="text" name="username" required><input type="textarea" name="address_line1" required max="60"><input type="textarea" name="address_line2"  max="60"><input type="text" name="zipcode" required><input type="text" name="phoneno" required> <input type="submit" name="address'+address_id+'" value="Save Address"></form>'; 
+		var planDiv = '<h3>Pickup Address '+address_id+'</h3> <form action="" method="POST" id="form_'+address_id+'"><input type="hidden" value="address '+address_id+'" placeholder="Name"><input type="text" name="username" required><input type="textarea" name="address_line1" required max="60" placeholder="Address 1"><input type="textarea" name="address_line2"  max="60" placeholder="Adrress 2"><input type="text" name="city" required placeholder="city"><input type="text" name="zipcode" placeholder="Zip Code" required><input type="text" name="phoneno" placeholder="Phone No" required> <input type="submit" name="address'+address_id+'" value="Save Address"></form>'; 
 		$("div[class^=addnewaddress]:last").after(planDiv);
 	});
 
