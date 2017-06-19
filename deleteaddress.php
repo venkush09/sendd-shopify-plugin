fd8b95080a6e614c67340d369ac88a0ef7d13f59
Anish-Jajodia <hello@lomdi.in>
2017-06-19 18:21:46 +0530

    Update deleteaddress.php

--- deleteaddress.php
+++ deleteaddress.php
@@ -1,11 +1,12 @@
 <?php 
 session_start();
 require __DIR__.'/connection.php'; //DB connectivity
-	$shop_url = $_POST['shop_url'];
+	
   if((isset($_REQUEST['id'])) || $_REQUEST['id']!=''){
+	  $shop_url = $_REQUEST['shop_url'];
     $id=$_REQUEST['id'];
-	  echo "DELETE FROM pickup_address WHERE shop_url='{$shop_url}' AND id ={id}";
-  $delete_address = pg_query($dbconn4,"DELETE FROM pickup_address WHERE shop_url='{$shop_url}' AND id ={id}");
+	  echo "DELETE FROM pickup_address WHERE shop_url='{$shop_url}' and id ={$id}";
+  $delete_address = pg_query($dbconn4,"DELETE FROM pickup_address WHERE shop_url='{$shop_url}' and id ={$id}");
   print_r($delete_address);
 	  
 	  echo "delete successfully";
