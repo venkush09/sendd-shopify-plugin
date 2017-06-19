429d2328c93fac606758c67ee665031affdb47fa
Anish-Jajodia <hello@lomdi.in>
2017-06-19 16:56:55 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -48,8 +48,14 @@ require __DIR__.'/connection.php'; //DB connectivity
 			 $pickup_address2 = pg_query($dbconn4, "SELECT id FROM pickup_address WHERE shop_url ='{$shop_url}' and id={$address_id}");
 			echo pg_num_rows($pickup_address2);
 			if(pg_num_rows($pickup_address2)){
-				 pg_query($dbconn4, "UPDATE pickup_address SET name ='{$name}' , address_line1='{$address_line1}', address_line2='{$address_line2}', city='{$city}', zipcode='{$zipcode}', phoneno='{$phoneno}',companyname='{$companyname}' WHERE shop_url='{$shop_url}' and id={$address_id}");
-			echo "Address update  sucessfully";
+				echo "UPDATE pickup_address SET name ='{$name}' , address_line1='{$address_line1}', address_line2='{$address_line2}', city='{$city}', zipcode='{$zipcode}', phoneno='{$phoneno}',companyname='{$companyname}' WHERE shop_url='{$shop_url}' and id={$address_id}";
+		           $sql =   pg_query($dbconn4, "UPDATE pickup_address SET name ='{$name}' , address_line1='{$address_line1}', address_line2='{$address_line2}', city='{$city}', zipcode='{$zipcode}', phoneno='{$phoneno}',companyname='{$companyname}' WHERE shop_url='{$shop_url}' and id={$address_id}");
+				if($qry){
+				echo "Address update  sucessfully";
+				}
+				else {
+				echo "ERROR";
+				}
 			}
 			else{
 			 $sql = "insert into pickup_address (name,address_line1,address_line2,city ,zipcode,phoneno,shop_url,companyname) values ('$name','$address_line1','$address_line2','$city', '$zipcode','$phoneno','$shop_url','$companyname' )";
