e6a583932527ed1c3f6933c1eff3963cf9de2631
Anish-Jajodia <hello@lomdi.in>
2017-06-19 17:46:59 +0530

    Create deleteaddress.php

---
+++ deleteaddress.php
@@ -0,0 +1,10 @@
+<?php 
+session_start();
+require __DIR__.'/connection.php'; //DB connectivity
+	$shop_url = $_POST['shop_url'];
+  if((isset($_REQUEST['id'])) || $_REQUEST['id']!=''){
+    $id=$_REQUEST['id'];
+  $delete_address = pg_query($dbconn4, "delete  FROM pickup_address WHERE store_url = '{$shop_url}' and  id ={id}");
+  echo "delete successfully";
+  }
+  ?>
