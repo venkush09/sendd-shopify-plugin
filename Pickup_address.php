ca8794e3b52a862d411e39377238c4303f85f18f
Anish-Jajodia <hello@lomdi.in>
2017-06-19 12:06:05 +0530

    Update Pickup_address.php

--- Pickup_address.php
+++ Pickup_address.php
@@ -11,12 +11,12 @@ $pickup_address = pg_query($dbconn4, "SELECT * FROM pickup_address WHERE shop_ur
 			<h3>Pickup Address <?php echo $i; ?></h3>
   <form action="" method="POST" id="form_<?php echo $i; ?>">
     <input name="saveaddress" type="hidden" value="<?php echo $i; ?>"><input type="hidden" name="shop_url" value="<?php echo $shop_url; ?>">
-    <input type="text" name="username" placeholder="Name" value="<?php echo $row['name']?>" required>
-    <textarea  name="address_line1" placeholder="Adrress 1" required max="60"><?php echo $row['address_line1']?></textarea>
-    <textarea name="address_line2" placeholder="Adrress 2"  max="60"><?php echo $row['address_line2']?></textarea>
-	<input value="<?php echo $row['city']?>" type="text" name="city" placeholder="City" required>
-    <input value="<?php echo $row['zipcode']?>" type="text" name="zipcode" placeholder="Zip Code" required>
-    <input value="<?php echo $row['phoneno']?>" type="text" name="phoneno" placeholder="Phone no" required>
+    <input type="text" name="username" placeholder="Name" value="<?php echo trim($row['name'])?>" required>
+    <textarea  name="address_line1" placeholder="Adrress 1" required max="60"><?php echo trim($row['address_line1']);?></textarea>
+    <textarea name="address_line2" placeholder="Adrress 2"  max="60"><?php echo trim($row['address_line2']);?></textarea>
+	<input value="<?php echo trim($row['city']);?>" type="text" name="city" placeholder="City" required>
+    <input value="<?php echo trim($row['zipcode']);?>" type="text" name="zipcode" placeholder="Zip Code" required>
+    <input value="<?php echo trim($row['phoneno']);?>" type="text" name="phoneno" placeholder="Phone no" required>
     <input value="Save Address" type="button" id="<?php echo $i ?>" class="savebtn" name="saveaddress" value="Save Address">
 			</form></div>
 		<?php
@@ -40,7 +40,7 @@ var address_id = parseInt($('body .formmain').length+1);
 	$('body').on('click', '.savebtn', function(e) {
 	   e.preventDefault();
 	   var get_id = $(this).attr('id');
-	   alert($("body #form_"+get_id+" input[name=username]").val());
+	   
 		if(($("body #form_"+get_id+" input[name=username]").val()!='') && ($("body #form_"+get_id+" input[name=address_line1]").val()!='')&& ($("body #form_"+get_id+" input[name=address_line2]").val()!='')&& ($("body #form_"+get_id+ "input[name=city]").val()!='')&& ($("body #form_"+get_id+" input[name=zipcode]").val()!='')&& ($("body #form_"+get_id+" input[name=phoneno1]").val()!='')){
 		
 		var formdata = $('body #form_'+get_id).serialize();
