2b8296343d82588b4943b4bf45690ab2f6128853
Anish-Jajodia <hello@lomdi.in>
2017-06-19 15:24:29 +0530

    Update get_pickupaddress.php

--- get_pickupaddress.php
+++ get_pickupaddress.php
@@ -8,7 +8,7 @@ if((isset($_REQUEST['pickupaddres'])) || $_REQUEST['pickupaddres']!=''){
 			 echo '<h5>Pickup Address*</h5><label>Pickup Company Name:</label><input type="text" class="p_company_name" value="">';
 			 echo '<label>Pickup address:</label><select name="pickup_address" class="pickup_address">';
 			while ($row = pg_fetch_assoc($pickup_address)) {
-				echo '<option data-companyname="'.trim($row['companyname']).'" value="'.trim($row['address_line1']).'" data-address2="'.trim($row['address_line2']).'" selected>'.trim($row['username']).':'.trim($row['address_line1']).','.trim($row['city']).'_'.trim($row['zipcode'])'.::'.trim($row['phoneno'])'</option>';
+				echo '<option data-companyname="'.trim($row['companyname']).'" value="'.trim($row['address_line1']).'" data-address2="'.trim($row['address_line2']).'" selected>'.trim($row['username']).':'.trim($row['address_line1']).','.trim($row['city']).'_'.trim($row['zipcode'])'.::'.trim($row['phoneno']).'</option>';
 			
 	   }
 		}
