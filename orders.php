97aadb1337a2aa254cb2e688e2d6f383782d879e
Anish-Jajodia <hello@lomdi.in>
2017-06-30 17:47:09 +0530

    Update orders.php

--- orders.php
+++ orders.php
@@ -39,6 +39,7 @@ try
 			foreach($orders as $singleorder)
 			{
 				$quantity_total=0;
+				$product_titles='';
 				 $id =$singleorder['id'];
 				 $name =$singleorder['name'];
 				 $created_at =$singleorder['created_at'];
@@ -73,11 +74,19 @@ try
 				}
 				$line_items=$singleorder['line_items'];
 				foreach($line_items as $line_items){
-						$quantity_total=$quantity_total+ $line_items['quantity']; //Image Source
+						$quantity_total=$quantity_total+ $line_items['quantity']; 
+						//Get product names
+						if($product_titles ==''){
+							$product_titles = $line_items['title'];
+						}
+						else{
+							$product_titles = $product_titles.','$line_items['title'];
+						}
+						
 					}
 				
 				echo "<tr>";
-				echo '<td><input  type="checkbox" $disabled1 class="select_box" name="order_ids_'.$id.'"  value="'.$id.'"  data-financial_status="'.$financial_status.'" data-total_weight="'.$total_weight.'" data-quantity_total="'.$quantity_total.'" data-customer_total-price="'.$total_price.'" data-customer_email="'.$email.'" data-customer_name="'.$customer_name.'" data-fulladdress="'.$full_address.'" data-gateway="'.$gateway.'" data-customer_phone="'.$customer_phone.'"></td>';
+				echo '<td><input  type="checkbox" $disabled1 class="select_box" name="order_ids_'.$id.'"  value="'.$id.'"  data-financial_status="'.$financial_status.'" data-total_weight="'.$total_weight.'" data-quantity_total="'.$quantity_total.'" data-customer_total-price="'.$total_price.'" data-customer_email="'.$email.'" data-customer_name="'.$customer_name.'" data-fulladdress="'.$full_address.'" data-gateway="'.$gateway.'" data-customer_phone="'.$customer_phone.'" data-products_nmae="'.$product_titles.'"></td>';
 				echo "<td>".$name."</td>";
 				echo "<td>".$created_at."</td>";
 				echo "<td>".$customer_name."</td>";
